b8287528b3736fb22bf70c9aa62bd17d9cd6adfb
WillieMaddox <willie.maddox@gmail.com>
2017-05-23 14:42:48 -0500

    update server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 var port = +process.env.PORT || 8050;
 var path = require('path');
+var errorHandler = require('express-error-handler');
 app.use(express.static(__dirname));
-app.use(express.errorHandler());
+app.use(errorHandler());
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
