8027feb7dab2c6cc39550f2903eaa691d6c6d24b
WillieMaddox <willie.maddox@gmail.com>
2017-07-06 02:29:27 -0500

    refactor

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const path = require('path');
-const errorHandler = require('express-error-handler');
+const express = require('express')
+const path = require('path')
+const errorHandler = require('express-error-handler')
 
-let app = express();
-let port = +process.env.PORT || 8050;
-app.use(express.static(__dirname));
-app.use(errorHandler());
+let app = express()
+let port = +process.env.PORT || 8050
+app.use(express.static(__dirname))
+app.use(errorHandler())
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
 
 app.listen(port, function () {
-    console.log('OSM Fire listening on port ' + port);
-});
+  console.log('OSM Fire listening on port ' + port)
+})
 
