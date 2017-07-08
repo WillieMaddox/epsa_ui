a5a279f286ddddb480c48effaf9258a18442d2bd
WillieMaddox <willie.maddox@gmail.com>
2017-07-08 02:23:48 -0500

    refactoring

--- src/main-js/WFS110Context.js
+++ src/main-js/WFS110Context.js
@@ -3,26 +3,12 @@
  */
 
 'use strict'
-import JsonixModule from 'jsonix'
-import xlink10Module from 'XLink_1_0'
-import wfs110Module from 'WFS_1_1_0'
-import filter110Module from 'Filter_1_1_0'
-import ows100Module from 'OWS_1_0_0'
-import smil20 from 'SMIL_2_0'
-import smil20lang from 'SMIL_2_0_Language'
-import gml311Module from 'GML_3_1_1'
 
-const Jsonix = JsonixModule.Jsonix
+import {Jsonix} from 'jsonix'
+import {XLink_1_0} from 'w3c-schemas'
+import {OWS_1_0_0, Filter_1_1_0, SMIL_2_0, SMIL_2_0_Language, GML_3_1_1, WFS_1_1_0} from 'ogc-schemas'
 
-const XLink_1_0 = xlink10Module.XLink_1_0
-const WFS_1_1_0 = wfs110Module.WFS_1_1_0
-const Filter_1_1_0 = filter110Module.Filter_1_1_0
-const OWS_1_0_0 = ows100Module.OWS_1_0_0
-const SMIL_2_0_Language = smil20lang.SMIL_2_0_Language
-const SMIL_2_0 = smil20.SMIL_2_0
-const GML_3_1_1 = gml311Module.GML_3_1_1
 const mappings = [SMIL_2_0_Language, SMIL_2_0, GML_3_1_1, XLink_1_0, Filter_1_1_0, OWS_1_0_0, WFS_1_1_0]
-
 const result = new Jsonix.Context(mappings)
 
 export default result
--- src/main-js/WFS200Context.js
+++ src/main-js/WFS200Context.js
@@ -3,20 +3,12 @@
  */
 
 'use strict'
-import JsonixModule from 'jsonix'
-import xlink10Module from 'XLink_1_0'
-import wfs20Module from 'WFS_2_0'
-import filter20Module from 'Filter_2_0'
-import ows110Module from 'OWS_1_1_0'
 
-const Jsonix = JsonixModule.Jsonix
+import {Jsonix} from 'jsonix'
+import {XLink_1_0} from 'w3c-schemas'
+import {OWS_1_1_0, Filter_2_0, GML_3_1_1, WFS_2_0} from 'ogc-schemas'
 
-const XLink_1_0 = xlink10Module.XLink_1_0
-const WFS_2_0 = wfs20Module.WFS_2_0
-const Filter_2_0 = filter20Module.Filter_2_0
-const OWS_1_1_0 = ows110Module.OWS_1_1_0
-const mappings = [OWS_1_1_0, XLink_1_0, Filter_2_0, WFS_2_0]
-
+const mappings = [OWS_1_1_0, XLink_1_0, Filter_2_0, GML_3_1_1, WFS_2_0]
 const result = new Jsonix.Context(mappings)
 
 export default result
