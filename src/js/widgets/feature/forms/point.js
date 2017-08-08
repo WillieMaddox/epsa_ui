d37924f86e9b62382774844ead3efd486bf4eb83
WillieMaddox <willie.maddox@gmail.com>
2017-08-08 07:49:05 -0500

    update geometry widgets to inherit from base geometry class.

--- src/js/widgets/feature/forms/point.js
+++ src/js/widgets/feature/forms/point.js
@@ -4,15 +4,17 @@
 
 'use strict'
 
-import $ from 'jquery'
+// import $ from 'jquery'
 import Name from '../nodes/name'
 import Type from '../nodes/featuretype'
 import point_templates from '../pointtemplates'
+import FeatureClass from '../featureclass'
 
-const form_elements = {}
+// const form_elements = {}
 
-class Point {
+class Point extends FeatureClass {
   constructor () {
+    super()
     this.type = 'point'
     this.form_nodes = {
       'name': new Name(point_templates),
@@ -23,43 +25,43 @@ class Point {
       ['type']
     ]
   }
-  createForm () {
-    const $form = $("<form id='featureproperties' class='form'>")
-    for (let label in this.form_nodes) {
-      form_elements[label] = this.form_nodes[label].createNode()
-    }
-    for (let row_node_labels of this.form_node_labels) {
-      $form.append(this.addFormRow(row_node_labels))
-    }
-    return $form
-  }
-  addFormRow (labels) {
-    const $formRow = $("<div class='form-row'>")
-    for (let label of labels) {
-      $formRow.append(form_elements[label])
-    }
-    return $formRow
-  }
-  styleForm () {
-    for (let label in this.form_nodes) {
-      this.form_nodes[label].styleNode()
-    }
-  }
-  loadFeature (feature) {
-    for (let label in this.form_nodes) {
-      this.form_nodes[label].loadFeature(feature)
-    }
-  }
-  saveFeature (feature) {
-    for (let label in this.form_nodes) {
-      this.form_nodes[label].saveFeature(feature)
-    }
-  }
-  deactivateForm () {
-    for (let label in this.form_nodes) {
-      this.form_nodes[label].deactivateNode()
-    }
-  }
+  // createForm () {
+  //   const $form = $("<form id='featureproperties'>")
+  //   for (let label in this.form_nodes) {
+  //     form_elements[label] = this.form_nodes[label].createNode()
+  //   }
+  //   for (let row_node_labels of this.form_node_labels) {
+  //     $form.append(this.addFormRow(row_node_labels))
+  //   }
+  //   return $form
+  // }
+  // addFormRow (labels) {
+  //   const $formRow = $("<div class='form-row'>")
+  //   for (let label of labels) {
+  //     $formRow.append(form_elements[label])
+  //   }
+  //   return $formRow
+  // }
+  // styleForm () {
+  //   for (let label in this.form_nodes) {
+  //     this.form_nodes[label].styleNode()
+  //   }
+  // }
+  // loadFeature (feature) {
+  //   for (let label in this.form_nodes) {
+  //     this.form_nodes[label].loadFeature(feature)
+  //   }
+  // }
+  // saveFeature (feature) {
+  //   for (let label in this.form_nodes) {
+  //     this.form_nodes[label].saveFeature(feature)
+  //   }
+  // }
+  // deactivateForm () {
+  //   for (let label in this.form_nodes) {
+  //     this.form_nodes[label].deactivateNode()
+  //   }
+  // }
 }
 
 export default Point
--- src/js/widgets/feature/forms/polygon.js
+++ src/js/widgets/feature/forms/polygon.js
@@ -4,20 +4,22 @@
 
 'use strict'
 
-import $ from 'jquery'
+// import $ from 'jquery'
 import Name from '../nodes/name'
 import Type from '../nodes/featuretype'
 import Hole from '../nodes/hole'
 import Measure from '../nodes/measure'
 // import OSMFire_Core from 'MainCore'
+import FeatureClass from '../featureclass'
 import polygon_templates from '../polygontemplates'
 
-const form_elements = {}
+// const form_elements = {}
 
 // const PolygonWidget = function (sandBox) {
 
-class Polygon {
+class Polygon extends FeatureClass {
   constructor () {
+    super()
     this.type = 'polygon'
     this.form_nodes = {
       'name': new Name(polygon_templates),
@@ -36,44 +38,51 @@ class Polygon {
     // sandBox.log(1, 'featuretype constructor() called...', 'blue')
 
   }
-  createForm () {
-    //TODO: use this when calling registerComponent
-    const $form = $("<form id='polygonproperties'>")
-    for (let label in this.form_nodes) {
-      form_elements[label] = this.form_nodes[label].createNode()
-    }
-    for (let row_node_labels of this.form_node_labels) {
-      $form.append(this.addFormRow(row_node_labels))
-    }
-    return $form
-  }
-  addFormRow (labels) {
-    const $formRow = $("<div class='form-row'>")
-    for (let label of labels) {
-      $formRow.append(form_elements[label])
-    }
-    return $formRow
-  }
-  styleForm () {
-    for (let node in this.form_nodes) {
-      this.form_nodes[node].styleNode()
-    }
-  }
-  loadFeature (feature) {
-    for (let node in this.form_nodes) {
-      this.form_nodes[node].loadFeature(feature)
-    }
-  }
-  saveFeature (feature) {
-    for (let node in this.form_nodes) {
-      this.form_nodes[node].saveFeature(feature)
-    }
-  }
-  deactivateForm () {
-    for (let node in this.form_nodes) {
-      this.form_nodes[node].deactivateNode()
-    }
-  }
+  // destroy (removeComponent) {
+  //   sandBox.unsubscribeFromAllCustomEvents()
+  //   if (removeComponent) {
+  //     sandBox.removeComponent('featureeditor')
+  //   }
+  //   sandBox.logMessage(1, 'Content component has been destroyed...', "blue")
+  // }
+  // createForm () {
+  //   //TODO: use this when calling registerComponent
+  //   const $form = $("<form id='polygonproperties'>")
+  //   for (let label in this.form_nodes) {
+  //     form_elements[label] = this.form_nodes[label].createNode()
+  //   }
+  //   for (let row_node_labels of this.form_node_labels) {
+  //     $form.append(this.addFormRow(row_node_labels))
+  //   }
+  //   return $form
+  // }
+  // addFormRow (labels) {
+  //   const $formRow = $("<div class='form-row'>")
+  //   for (let label of labels) {
+  //     $formRow.append(form_elements[label])
+  //   }
+  //   return $formRow
+  // }
+  // styleForm () {
+  //   for (let node in this.form_nodes) {
+  //     this.form_nodes[node].styleNode()
+  //   }
+  // }
+  // loadFeature (feature) {
+  //   for (let node in this.form_nodes) {
+  //     this.form_nodes[node].loadFeature(feature)
+  //   }
+  // }
+  // saveFeature (feature) {
+  //   for (let node in this.form_nodes) {
+  //     this.form_nodes[node].saveFeature(feature)
+  //   }
+  // }
+  // registerForEvents () {
+  //   for (let node in this.form_nodes) {
+  //     this.form_nodes[node].registerForEvents()
+  //   }
+  // }
 }
 // return Polygon
 
